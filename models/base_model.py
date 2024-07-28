a60b3f016de47952d118e3534aa99a2a24601fc7
buyekeobare <buyekeobare@gmail.com>
2024-07-28 07:28:25 -0700

    base_model.py updated

--- models/base_model.py
+++ models/base_model.py
@@ -1,34 +1,40 @@
 #!/usr/bin/python3
-"""This module is the base class for all models in our Airbnbclone."""
+"""This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
-    """Base class for all airbnb models"""
+    """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
+        """Instantiates a new model"""
+        if (not kwargs) or ('updated_at' not in kwargs):
             from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+
+            if kwargs:
+                for key, value in kwargs.items():
+                    setattr(self, key, value)
+
             storage.new(self)
+
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
+                                                         '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
+                                                         '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
             self.__dict__.update(kwargs)
 
     def __str__(self):
-        """Returns a str representation of an instance"""
+        """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
-        """Updates updated_at with curr time when instance is changed"""
+        """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
         storage.save()
